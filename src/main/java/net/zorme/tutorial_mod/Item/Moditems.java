71b0b6da6f51646fc064f8105606527d3e5a0d69
Generic-Adaptagon <zorme@startmail.com>
2024-07-24 20:33:54 -0700

    Allowed Beaper to set entities whom eat it on fire

--- src/main/java/net/zorme/tutorial_mod/Item/Moditems.java
+++ src/main/java/net/zorme/tutorial_mod/Item/Moditems.java
@@ -5,7 +5,10 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import net.zorme.tutorial_mod.Item.custom.BeaperItem;
 import net.zorme.tutorial_mod.Item.custom.FuelItem;
+
 import net.zorme.tutorial_mod.Item.custom.MetalDetectorItem;
 import net.zorme.tutorial_mod.tutorial_mod;
 
@@ -21,10 +24,11 @@ public class Moditems {
     public static final RegistryObject <Item>  DEBIT_CARD = ITEMS.register("debit_card", () -> new  Item(new Item.Properties()));
 
     public static final RegistryObject <Item>  METAL_DETECTOR = ITEMS.register("metal_detector", () -> new MetalDetectorItem(new Item.Properties().durability(100)));
+    public static final RegistryObject <Item>  BEAPER = ITEMS.register("beaper", () -> new BeaperItem(new Item.Properties().food(ModFoods.BEAPER)));
 
     public static final RegistryObject <Item>  STRAWBERRY = ITEMS.register("strawberry", () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
     public static final RegistryObject <Item>  SAPPHIRE_STAFF = ITEMS.register("sapphire_staff", () -> new Item(new Item.Properties().stacksTo(1)));
-    public static final RegistryObject <Item>  BEAPER = ITEMS.register("beaper", () -> new  Item(new Item.Properties().food(ModFoods.BEAPER).stacksTo(64)));
+    //public static final RegistryObject <Item>  BEAPER = ITEMS.register("beaper", () -> new  Item(new Item.Properties().food(ModFoods.BEAPER)));
     public static final RegistryObject <Item>  BLUE_BEAN = ITEMS.register("blue_bean", () -> new  Item(new Item.Properties().food(ModFoods.BLUE_BEAN)));
     public static final RegistryObject <Item> PINE_CONE = ITEMS.register("pine_cone", () -> new FuelItem(new Item.Properties(), 400));
     public static final RegistryObject <Item> SAPPHIRE_SWORD = ITEMS.register("sapphire_sword", () -> new SwordItem(ModToolTeirs.SAPPHIRE, 4, 2,new Item.Properties()));
---
+++ src/main/java/net/zorme/tutorial_mod/Item/custom/BeaperItem.java
@@ -0,0 +1,38 @@
+package net.zorme.tutorial_mod.Item.custom;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class BeaperItem extends Item{
+
+    public BeaperItem(Item.Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity) {
+
+        pLivingEntity.setSecondsOnFire(5);
+
+        return super.finishUsingItem(pStack, pLevel, pLivingEntity);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+
+        pTooltipComponents.add(Component.translatable("tooltip.tutorial_mod.beaper.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
+
+}
